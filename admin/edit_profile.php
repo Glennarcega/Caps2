a166d6c37c2f57e3093e799baaf7401a9074353b
Glennarcega <glennarcega177@gmail.com>
2023-10-02 21:24:57 +0800

    update settings 1.3

--- admin/edit_profile.php
+++ admin/edit_profile.php
@@ -112,31 +112,60 @@ if(isset($_SESSION['user_data'])){
               <div class="col-md-3 border-right">
               <div class="row mt-3">
           <div class="col-md-12">
-          <form method = "POST" enctype="multipart/form-data">
+          <form method = "POST" enctype = "multipart/form-data">
               <label class="labels"><br>Change Avatar</label>
               <div class = "well" style = "height:200px; width:100%;">
                   <img src = "../photo/<?php echo $_SESSION['user_data']['photo']?>" height = "160" width = "225"/>
                   </div>
+                  <input type="file" name="photo" id="photo" class="form-control" value="../photo/<?php echo $_SESSION['user_data']['photo']?>">
           </div>  
         </div>           
       </div>
-      <div class="col-md-5 border-right">
-            <div class="p-3 py-5">
-              <div class="row mt-2">
-                <div class="col-md-6"><label class="labels">Name</label><label class="form-control"><?php echo $_SESSION['user_data']['fname']; ?></label></div>
-                <div class="col-md-6"><label class="labels">Last Name</label><label class="form-control"><?php echo $_SESSION['user_data']['lname']; ?></label></div>
-                <div class="col-md-6"><label class="labels"><br>Email</label><label class="form-control"><?php echo $_SESSION['user_data']['username']; ?></label></div>
+          <div class="col-md-5 border-right">
+              <div class="p-3 py-5">
+                <div class="row mt-2">
+                  <div class="col-md-6"><label class="labels">Name</label><input type="text" name="fname" value = "<?php echo $_SESSION['user_data']['fname']; ?>" class="form-control" placeholder="First Name" required></div>
+                  <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" name="lname" value = "<?php echo $_SESSION['user_data']['lname']; ?>" placeholder="Last Name" required></div>
+                  <div class="col-md-6"><label class="labels"><br>Email</label><input type="text" name="username" value = "<?php echo $_SESSION['user_data']['username']; ?>" class="form-control" value="" placeholder="Example@gmail.com" required></div>
               </div>
-              <div class="row mt-3">
-                <div class="col-md-6"><label class="labels"><br>Mobile Number</label><label class="form-control"><?php echo $_SESSION['user_data']['mobile_number']; ?></label></div>
-                <div class="col-md-12"><label class="labels"><br>Address</label><label class="form-control"><?php echo $_SESSION['user_data']['address']; ?></label></div>
+                    <div class="row mt-3">
+                    <div class="col-md-6"><label class="labels"><br>Mobile Number</label><input type="text" name="mobile_number" value = "<?php echo $_SESSION['user_data']['mobile_number']; ?>" class="form-control" placeholder="Ex.0946" required></div>
+                    <div class="col-md-12"><label class="labels"><br>Address</label><input type="text" name="address" value = "<?php echo $_SESSION['user_data']['address']; ?>" class="form-control" placeholder="Address" required></div>
               </div>
-              <br>
-              <div class="mt-5 text-center">
-              <div class="mt-5 text-center">
-              <a href="edit_profile.php?id=<?php echo $_SESSION['user_data']['id']; ?>" class="btn btn-primary profile-button">Edit Profile</a>
+                <br>
+                    <div class="mt-5 text-center"> <button type = "submit" name="submit" class = "btn btn-primary profile-button"> Save Profile</button></div>
+              </div>
+            </form>
+
+                <?php
+                   if (isset($_POST['submit'])) {
+                    $fname = $_POST['fname'];
+                    $lname = $_POST['lname'];
+                    $address = $_POST['address'];
+                    $mobile_number = $_POST['mobile_number'];
+                    $username = $_POST['username'];
+                
+                    // Check if a new photo was uploaded
+                    if (!empty($_FILES['photo']['tmp_name'])) {
+                        $photo = addslashes(file_get_contents($_FILES['photo']['tmp_name']));
+                        $photo_name = addslashes($_FILES['photo']['name']);
+                        $photo_size = getimagesize($_FILES['photo']['tmp_name']);
+                        move_uploaded_file($_FILES['photo']['tmp_name'], "../photo/" . $_FILES['photo']['name']);
+                    } else {
+                        // If no new photo was uploaded, retain the existing photo
+                        $photo = $_SESSION['user_data']['photo'];
+                        $photo_name = $_SESSION['user_data']['photo'];
+                    }
+                
+                    $query = $conn->query("UPDATE `users` SET `fname` = '$fname', `lname` ='$lname', `address` = '$address', `mobile_number` = '$mobile_number', `username` = '$username', `photo` = '$photo_name' WHERE `id` = '$_REQUEST[id]'") or die(mysqli_error());
+                    
+                    echo '<script>alert("Update Successfully. Click OK to logout and Login again to see changes.");</script>';
+    
+                    // Automatically redirect to the logout page
+                    echo '<script>window.location.href = "../index.php";</script>';                }
+                
+                  ?>
                 </div>
-              </div>
                 </div>
               </div>
             </div>
@@ -145,6 +174,7 @@ if(isset($_SESSION['user_data'])){
       </div>
   </section>
 </body>
+<!-- Scripts -->
 <script src="../cssmainmenu/script.js"></script>
 <script>
 document.addEventListener("DOMContentLoaded", function() {

3b4030059bcc0f40f532a1f62cf2f1de997a3877
Glennarcega <glennarcega177@gmail.com>
2023-10-27 15:20:21 +0800

    reset pass may error pa

--- reset-password.php
+++ reset-password.php
@@ -35,12 +35,123 @@ if (strtotime($user["reset_token_expires_at"]) <= time()) {
 <head>
     <title>Login</title>
      <!-- custom css file link -->
+     <meta name="viewport" content="width=device-width, initial-scale=1">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossorigin="anonymous">
     <link rel="stylesheet" href="csslog/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+.password-container {
+        position: relative;
+    }
+
+.toggle-password-btn {
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+.form-field input {
+    width: 100%;
+    display: block;
+    border: none;
+    outline: none;
+    font-size: 1.2rem;
+    color: #666;
+    padding: 10px 15px 10px 10px;
+    border-radius: 20px;
+}
+
+.form-field {
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    margin-bottom: 20px;
+    border-radius: 20px;
+    background-color: #fff;
+}
+
+.form-field input {
+    flex: 1;
+    width: 100%;
+    border: none;
+    outline: none;
+    background: none;
+    font-size: 1.2rem;
+    color: #666;
+    padding: 10px 15px 10px 10px;
+    margin-left:30px;
+}
+
+.input-icon {
+    position: relative;
+    display: flex;
+    align-items: center;
+}
+
+.input-icon span {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 1.2rem;
+    color: #555;
+}
+
+.toggle-password {
+    cursor: pointer;
+    transition: color 0.3s;
+    margin-left: -30px; /* I-adjust ang margin para ilagay ang icon sa kanan ng password field */
+}
+
+.toggle-password.active {
+    color: #555; /* Baguhin ang kulay ng mata icon kapag ito ay active (password visible) */
+}
+#emailError {
+    color: red;
+    display: none;
+    margin-top: -20px;
+}
+
+
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+    
+
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  text-align: center;
+  background-image: url("./img/opening.jpg");
+  font-size: 16px;
+    display: block;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 20px;
+    text-align: center;
+}
+
+
+
+</style>
 </head>
-<body class =bodyy>
+
+<body>
+<div class="column">
+        <div class="card">
 <div class="form-container">
     <form method="post" class="mx-auto" method="post" action="process-reset-password.php"
           style="background-color: #FFFACD">
@@ -59,22 +170,87 @@ if (strtotime($user["reset_token_expires_at"]) <= time()) {
         <?php } ?>
 
         <input type="hidden" name="token" value="<?= htmlspecialchars($token) ?>">
-        <input type="password" required placeholder="Enter New Password"
-               class="form-control"
-               name="password" id="password">
-
-        <input type="password" required placeholder="Confirm Password"
-               class="form-control"
-               name="password_confirmation" id="password_confirmation">
-        <div class="password-container">            
+        <div class="form-field d-flex align-items-center">
+            <div class="input-icon">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="password" oninput="togglePasswordButton('passwordToggle')" autocomplete="off" required placeholder="Enter New Password" style="font-size: 15px; height: 50px; width: 255px;" />
+                <button type="button" id="passwordToggle" class="toggle-password-btn" onclick="togglePasswordVisibility('password')">
+                    <i class="fas fa-eye-slash toggle-password"></i>
+                </button>
+            </div>
+            <p id="password-validation-msg" class="form-text text-danger"></p> <!-- Add a paragraph for validation messages -->
         </div>
+        
+
+        <div class="form-field d-flex align-items-center">
+            <div class="input-icon">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password_confirmation" id="password_confirmation" oninput="togglePasswordButton('passwordConfirmationToggle')" autocomplete="off" required placeholder="Confirm Password" style="font-size: 15px; height: 50px; width: 255px;" />
+                <button type="button" id="passwordConfirmationToggle" class="toggle-password-btn" onclick="togglePasswordVisibility('password_confirmation')">
+                    <i class="fas fa-eye-slash toggle-password"></i>
+                </button>
+            </div>
+        </div>
+
         <br>
         <button class="btn btn-primary mt-0">Send</button>
         <br>
-    
-        </form>
     </form>
 </div>
-   
 </body>
+
+<script>
+function togglePasswordButton(inputId) {
+    var passwordInput = document.getElementById(inputId);
+    var showPasswordBtn = document.getElementById(inputId + "Toggle");
+
+    if (passwordInput.value.length > 0) {
+        showPasswordBtn.style.display = "block";
+    } else {
+        showPasswordBtn.style.display = "none";
+    }
+}
+
+function togglePasswordVisibility(inputId) {
+    var passwordInput = document.getElementById(inputId);
+    var showPasswordBtn = document.getElementById(inputId + "Toggle");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPasswordBtn.querySelector("i").classList.remove("fa-eye-slash");
+        showPasswordBtn.querySelector("i").classList.add("fa-eye");
+    } else {
+        passwordInput.type = "password";
+        showPasswordBtn.querySelector("i").classList.remove("fa-eye");
+        showPasswordBtn.querySelector("i").classList.add("fa-eye-slash");
+    }
+}
+</script>
+
+<script>
+       function validatePassword() {
+    const passwordInput = document.getElementById("password");
+    const password = passwordInput.value;
+    const passwordValidationMsg = document.getElementById("password-validation-msg");
+    const submitButton = document.getElementById("submit-button"); // Get the submit button.
+
+    // Define a regular expression pattern for allowed characters.
+    const allowedCharacters = /^[a-zA -Z0-9 /^!@#$%^&*()\-_=+\{}|;:,<.>\/?+$/]+$/;
+
+    if (password.length < 8) {
+        passwordValidationMsg.textContent = "Password must be at least 8 characters long.";
+        submitButton.disabled = true; // Disable the submit button.
+        return false; // Prevent form submission
+    } else if (!allowedCharacters.test(password)) {
+        passwordValidationMsg.textContent = "Password contains disallowed that characters.";
+        submitButton.disabled = true; // Disable the submit button.
+        return false; // Prevent form submission
+    } else {
+        passwordValidationMsg.textContent = ""; // Clear any previous validation message.
+        submitButton.disabled = false; // Enable the submit button.
+        return true; // Allow form submission
+    }
+}
+
+    </script>
 </html>
